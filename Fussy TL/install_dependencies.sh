36ecaa91f23003706761656ac40dacd530c78e33
Mateusz Żarski <zski.mateusz@gmail.com>
2021-01-13 16:45:50 +0100

    Create install_dependencies.sh

---
+++ Fussy TL/install_dependencies.sh
@@ -0,0 +1,11 @@
+python3 -m pip install tensorflow>=1.14.0
+# for GPU support uncomment following line
+#python3 -m pip install tensorflow-gpu
+python3 -m pip install keras==2.3.0
+python3 -m pip install cv2
+python3 -m pip install imutils
+python3 -m pip install h5py==2.10.0
+python3 -m pip install numpy
+python3 -m pip install progressbar2
+python3 -m pip install sklearn
+python3 -m pip install matplotlib
